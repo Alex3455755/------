49d7d7fc85400e95bae3e22e3d4a40e889b0dee5
Alex3455755 <alexeigyll@gmail.com>
2023-03-23 17:40:46 +0500

    form for sign

--- quiz/src/pages/components/menu.jsx
+++ quiz/src/pages/components/menu.jsx
@@ -39,14 +39,20 @@ class ModalLogin extends React.Component {
     }
 }
 class ModalDefault extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {loginValid: false,passwordValid: false,disabledBtn: true}
+    }
     sendForm = () => {
         fetch('/form', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: JSON.stringify({ login: document.getElementById('login').value,
-             password: document.getElementById('password').value }),
+            body: JSON.stringify({
+                login: document.getElementById('login').value,
+                password: document.getElementById('password').value
+            }),
         }).then((res) => res.json())
             .then((data) => {
                 console.log(data)
@@ -55,12 +61,40 @@ class ModalDefault extends React.Component {
                 }
             })
     }
+    validateInput = (event) => {
+        let obj = 'passwordValid';
+        if(event.target.id === 'login'){
+            obj = 'loginValid';
+        }
+        if(event.target.value.length < 8){
+            event.target.classList.remove('text-field__input_valid')
+            event.target.classList.add('text-field__input_invalid');
+        }else{
+            event.target.classList.remove('text-field__input_invalid');
+            event.target.classList.add('text-field__input_valid');
+            this.setState({[obj]: true});
+            if (this.state.loginValid && this.state.passwordValid){
+                this.setState({disabledBtn: false})
+            }
+        }
+    }
     render() {
         return (
             <div className='modal modalDefault'>
-                <input type="text" placeholder='Логин' id='login' />
-                <input type="text" placeholder='Пароль' id='password' />
-                <button onClick={this.sendForm}>Войти</button>
+                <div className="text-field__wrapper">
+                    <div className="text-field text-field_floating-2">
+                        <input className="text-field__input" type="text" name="city" id="login" placeholder="Moscow" onChange={this.validateInput} />
+                            <label className="text-field__label" htmlFor="city">Логин</label>
+                    </div>
+                </div>
+                <div className="text-field__wrapper">
+                    <div className="text-field text-field_floating-2">
+                        <input className="text-field__input" type="text" name="username" id="password" placeholder='login'onChange={this.validateInput} />
+                            <label className="text-field__label" htmlFor="username">Пароль</label>
+                    </div>
+                </div>
+                <button onClick={this.sendForm} disabled={this.state.disabledBtn} id='btnSend'>Войти</button>
+                <button>Зарегестритоватся</button>
             </div>
         )
     }
