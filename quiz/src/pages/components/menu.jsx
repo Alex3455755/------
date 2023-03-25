36e1682b22eb4235b65d2466ef686f7621aa1977
Alex3455755 <alexeigyll@gmail.com>
2023-03-25 23:20:27 +0500

    conect database and seting registration

--- quiz/src/pages/components/menu.jsx
+++ quiz/src/pages/components/menu.jsx
@@ -38,10 +38,81 @@ class ModalLogin extends React.Component {
         )
     }
 }
-class ModalDefault extends React.Component {
-    constructor(props){
+class ModalRegistred extends React.Component {
+    constructor(props) {
         super(props)
-        this.state = {loginValid: false,passwordValid: false,disabledBtn: true}
+        this.fn = props.fn;
+        this.state = { loginValid: '', passwordValid: '', disabledBtn: true }
+    }
+    sendForm = () => {
+        fetch('/form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                login: document.getElementById('loginReg').value,
+                password: document.getElementById('passwordReg').value
+            }),
+        }).then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                if (data.succes) {
+                    window.userSign = true;
+                }
+            })
+    }
+    validInput = ({ target }) => {
+        if (target.type === 'text') {
+            const a = 'loginValid';
+            if (target.value.length < 7) {
+                console.log('login')
+                console.log(a)
+                this.setState({ [a]: 'text-field__input_invalid' });
+            } else {
+                this.setState({ [a]: 'text-field__input_valid' });
+            }
+        }
+        else {
+            const a = 'passwordValid';
+            if (/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9!@#$%^&*a-zA-Z]{5,}/g.test(target.value)) {
+                this.setState({ passwordValid: 'text-field__input_valid' });
+            } else {
+                this.setState({ passwordValid: 'text-field__input_invalid' });
+            }
+        }
+        if (this.state.loginValid === 'text-field__input_valid' && this.state.passwordValid === 'text-field__input_valid') {
+            this.setState({ disabledBtn: false });
+        }
+        else { this.setState({ disabledBtn: true }) }
+    }
+    render() {
+        return (
+            <div className='modal modalDefault modalReg'>
+                <div className="text-field__wrapper">
+                    <div className="text-field text-field_floating-2">
+                        <div className="text-field__message">Пароль должен состоять из букв латинского алфавита цифр и специальных символов</div>
+                        <input className={"text-field__input " + this.state.loginValid} type="text" name="city" id="loginReg" placeholder="Moscow" onChange={this.validInput} />
+                        <label className="text-field__label" htmlFor="city">Логин</label>
+
+                    </div>
+                </div>
+                <div className="text-field__wrapper">
+                    <div className="text-field text-field_floating-2">
+                        <input className={"text-field__input " + this.state.passwordValid} type="password" name="username" id="passwordReg" placeholder='login' onChange={this.validInput} />
+                        <label className="text-field__label" htmlFor="username">Пароль</label>
+                    </div>
+                </div>
+                <button onClick={this.sendForm} disabled={this.state.disabledBtn} id='btnSend'>Зарегестритоватся</button>
+                <button onClick={this.fn}>Войти</button>
+            </div>
+        )
+    }
+}
+class ModalDefault extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { loginValid: false, passwordValid: false, disabledBtn: true, showRegistr: false }
     }
     sendForm = () => {
         fetch('/form', {
@@ -61,20 +132,26 @@ class ModalDefault extends React.Component {
                 }
             })
     }
+    callBack = () => {
+        this.setState({ showRegistr: false });
+    }
+    setVisReg = () => {
+        this.setState({ showRegistr: !this.state.showRegistr });
+    }
     validateInput = (event) => {
         let obj = 'passwordValid';
-        if(event.target.id === 'login'){
+        if (event.target.id === 'login') {
             obj = 'loginValid';
         }
-        if(event.target.value.length < 8){
+        if (event.target.value.length < 7) {
             event.target.classList.remove('text-field__input_valid')
             event.target.classList.add('text-field__input_invalid');
-        }else{
+        } else {
             event.target.classList.remove('text-field__input_invalid');
             event.target.classList.add('text-field__input_valid');
-            this.setState({[obj]: true});
-            if (this.state.loginValid && this.state.passwordValid){
-                this.setState({disabledBtn: false})
+            this.setState({ [obj]: true });
+            if (this.state.loginValid && this.state.passwordValid) {
+                this.setState({ disabledBtn: false })
             }
         }
     }
@@ -84,17 +161,18 @@ class ModalDefault extends React.Component {
                 <div className="text-field__wrapper">
                     <div className="text-field text-field_floating-2">
                         <input className="text-field__input" type="text" name="city" id="login" placeholder="Moscow" onChange={this.validateInput} />
-                            <label className="text-field__label" htmlFor="city">Логин</label>
+                        <label className="text-field__label" htmlFor="city">Логин</label>
                     </div>
                 </div>
                 <div className="text-field__wrapper">
                     <div className="text-field text-field_floating-2">
-                        <input className="text-field__input" type="text" name="username" id="password" placeholder='login'onChange={this.validateInput} />
-                            <label className="text-field__label" htmlFor="username">Пароль</label>
+                        <input className="text-field__input" type="password" name="username" id="password" placeholder='login' onChange={this.validateInput} />
+                        <label className="text-field__label" htmlFor="username">Пароль</label>
                     </div>
                 </div>
                 <button onClick={this.sendForm} disabled={this.state.disabledBtn} id='btnSend'>Войти</button>
-                <button>Зарегестритоватся</button>
+                <button onClick={this.setVisReg}>Зарегестритоватся</button>
+                {this.state.showRegistr ? <ModalRegistred fn={this.callBack} /> : ''}
             </div>
         )
     }
--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const mysql = require("mysql2");
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
 const secret = 'bipE';
+  
+/* const connection = mysql.createConnection({
+  host: "192.168.0.52",
+  user: "laptop",
+  database: "Quiz",
+  password: "MySql:65108bipE;"
+}); */
 
 app.post('/form', jsonParser, (req, res) => {
     console.log(req.body);
     res.json({mesage: 'ok'});
 });
+
+/* connection.query('SELECT * FROM users',(err,data)=>{
+    console.log(data);
+}); */
 
 
 app.get('/', (req, res) => {
